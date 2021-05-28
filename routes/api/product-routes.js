41e4a8a647dbbfa92c4385aed9529e0fcb9ea960
Punkinut <heypunkinut@gmail.com>
2021-05-28 17:25:01 +1000

    You are now able to delete products by putting the id as a parameter

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
     const getProduct = await Product.findByPk(req.params.id, {
       include: [{ model: Category }, { model: Tag }],
@@ -25,20 +25,10 @@ router.get('/:id', (req, res) => {
   } catch {
     return res.status(400).json(err)
   }
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -103,8 +93,17 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleteProduct = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+    return res.status(200).json(deleteProduct)
+  } catch {
+    return res.status(400).json(err)
+  }
 });
 
 module.exports = router;
