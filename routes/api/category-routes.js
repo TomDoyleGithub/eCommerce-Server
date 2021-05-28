b6e492866a9d446e0c2eec784f6e14daf7f79973
Punkinut <heypunkinut@gmail.com>
2021-05-28 15:28:47 +1000

    Added post request that adds categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -25,7 +25,17 @@ router.get('/:id', async (req, res) => {
   };
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const newCategory = Category.create(
+      {
+        category_name: req.body.category_name,
+      }
+    )
+    return res.status(200).json(newCategory)
+  } catch {
+    return res.status(400).json(err)
+  }
   // create a new category
 });
 
