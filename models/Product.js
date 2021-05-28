ba9d6638fb654048a3da3449643c8337b11c4670
Punkinut <heypunkinut@gmail.com>
2021-05-28 10:25:15 +1000

    Added product name and price column to Product table

--- models/Product.js
+++ models/Product.js
@@ -12,6 +12,17 @@ Product.init(
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
     }
   },
   {
