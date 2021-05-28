6c67adba83b1c0ffe4a53c006ba92db64e262864
Punkinut <heypunkinut@gmail.com>
2021-05-28 11:27:22 +1000

    Added category and product realtion

--- models/index.js
+++ models/index.js
@@ -4,7 +4,14 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Category.hasOne(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
 
